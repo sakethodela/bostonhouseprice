a5f4e2a4b83b37d8043b7d1df1457e4b422497b7
sakethodela <sakethodelad@gmail.com>
2025-07-24 14:28:00 +0530

    committing all files into git

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # bostonhouseprice
+
+step1: create a new environment
+ ```
+ conda create -p venv python==3.11 -y
+
+```
+ step2:install requires libraries stored in requiremnents.txt file
+ step3: configure git
+     1. git config --global user.name/email
+     2. add files to git account 
+        ```
+          git add filename
+          git add . --all files gets added 
+         ```
+     3. to know status
+        ```
+           git status
+        ```
+     4. add file need to be committed to do
+       ```
+          git commit -m "message"
+       ```
+     5.final is push into git
+       ```
+          
---
+++ app.py
@@ -0,0 +1,30 @@
+import pickle
+import numpy as np
+import pandas as pd
+from flask import Flask,request,app,jsonify,url_for,render_template
+#create a app using flask framework
+app = Flask(__name__)
+#load model using pickle file
+regmodel=pickle.load(open('regression_model.pkl','rb'))
+scaler=pickle.load(open('scaling.pkl','rb'))
+#route to web i.e., after localhost /
+@app.route('/')
+#we need to route to home page on web so, create a method
+def home():
+    return render_template("home.html")
+#create an api becuase using api calls our model predict output
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    data=request.json['data'] #request for data which in json format key&value pair
+    print(data)
+    #convert list to values, convert to array, reshape because in our regression problem data in 2-D, so, we are converting them.
+    print(np.array(list(data.values())).reshape(1,-1))
+    #we collet data need to standardze so, for each fetaure record it produces mutliple datapoints as rows so, we are converting them into features as final for each record has multiple datapoints as features.
+    new_data = scaler.transform(np.array(list(data.values())).reshape(1,-1))
+    #predict the data using our model
+    output=regmodel.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+# runnng our created name app by making conditions like euqls to main app
+if __name__ == "__main__":
+    app.run(debug=True)
